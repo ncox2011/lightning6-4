8bc00bb886a165aa5a0396bd9c3b7f0d643851f9
Natasha Cox <natashac1014@gmail.com>
2018-06-04 09:19:35 -0500

    task complete

---
+++ main.js
@@ -0,0 +1,10 @@
+const lastName = "Cox"
+//Create a function to count the letters in the string
+
+if (lastName.length >= 7) {
+        console.log("I can't pronounce this last name");
+    } else {
+        console.log(lastName);
+    }
+
+
